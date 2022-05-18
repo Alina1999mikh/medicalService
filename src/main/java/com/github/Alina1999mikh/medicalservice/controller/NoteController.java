77f7ee05321f85bac6a1863cd8e4190432036765
Alina1999mikh <deswier@bk.ru>
2022-05-18 16:50:00 +0700

    Autorization

--- src/main/java/com/github/Alina1999mikh/medicalservice/controller/NoteController.java
+++ src/main/java/com/github/Alina1999mikh/medicalservice/controller/NoteController.java
@@ -3,6 +3,8 @@ package com.github.Alina1999mikh.medicalservice.controller;
 import com.github.Alina1999mikh.medicalservice.dto.CreateNoteRequest;
 import com.github.Alina1999mikh.medicalservice.dto.NoteResponse;
 import com.github.Alina1999mikh.medicalservice.service.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,28 +21,29 @@ import java.util.UUID;
 public class NoteController {
     private final NoteService noteService;
 
+    @Operation(summary = "create", security = @SecurityRequirement(name="basicAuth"))
     @PostMapping
     ResponseEntity<?> createNote(@Validated @RequestBody CreateNoteRequest request) {
         noteService.createNote(request);
         return ResponseEntity.created(URI.create("/v1/note/" + request.getUuid())).build();
     }
 
+    @Operation(summary = "get", security = @SecurityRequirement(name="basicAuth"))
     @GetMapping("/{uuid}")
     ResponseEntity<NoteResponse> findNoteByUuid(@PathVariable UUID uuid) {
         return ResponseEntity.of(noteService.findNoteByUuid(uuid));
     }
 
-//    @GetMapping("/{test}")
-//    ResponseEntity<List<NoteResponse>> findNotesByTest(@PathVariable String test) {
-//    }
-
+    @Operation(summary = "get", security = @SecurityRequirement(name="basicAuth"))
     @GetMapping("/")
     ResponseEntity<List<NoteResponse>> getNotes(Optional<String> test) {
         if (test.isPresent()) return ResponseEntity.ok(noteService.findNotesByTest(test.get()));
         else return ResponseEntity.ok(noteService.getAllNotes());
     }
 
-    @DeleteMapping("/{uuid}")
+    @Operation(summary = "del", security = @SecurityRequirement(name="basicAuth"))
+    @DeleteMapping("/" +
+            "delete/{uuid}")
     ResponseEntity<?> deleteNoteByUuid(@PathVariable UUID uuid) {
         noteService.deleteNoteByUuid(uuid);
         return ResponseEntity.ok().build();
